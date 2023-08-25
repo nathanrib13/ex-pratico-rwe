bc5d19669e7ff9fdd493921bf933c9254a5f1b38
nathanrib13 <nathan.ribeiros7@gmail.com>
2023-08-25 18:01:13 -0300

    register finished and swagger added

--- back/src/app.ts
+++ back/src/app.ts
@@ -4,14 +4,16 @@ import { loginRoutes } from "./routes/login.routes";
 import { usersRoutes } from "./routes/users.routes";
 import { imageRoutes } from "./routes/image.routes";
 import { handleErrors } from "./errors";
+import swaggerDocument from "./swagger.json";
 import express = require("express");
 import cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
 
 const app: express.Application = express();
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static("uploads"));
-
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/login", loginRoutes);
 app.use("/users", usersRoutes);
 app.use("/image", imageRoutes);
